b80e694d1e8865fb996e0010ea96c3b1a8fea1c4
Brandon Wallace <brandonwallace403@gmail.com>
2023-09-30 01:15:26 -0400

    frontend & backend working

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,29 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
-  return <h1>Project Client</h1>;
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5555/messages")
+      .then((r) => r.json())
+      .then((messages) => setMessages(messages));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>me ans me </h1>
+      {messages.map(message => {
+        return (
+          <div key = {message.id}>
+            <h2>{message.body}</h2>
+            <p>{message.username}</p>
+            <p>{message.created_at}</p>
+
+            </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default App;
