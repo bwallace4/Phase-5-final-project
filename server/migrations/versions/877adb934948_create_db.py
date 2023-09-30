6588687ad93746f9be8fc29a4baecfe98fcdf719
Brandon Wallace <brandonwallace403@gmail.com>
2023-09-29 20:35:46 -0400

    create empty migration for database

---
+++ server/migrations/versions/877adb934948_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: 877adb934948
+Revises: 
+Create Date: 2023-09-29 20:31:53.487699
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '877adb934948'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
