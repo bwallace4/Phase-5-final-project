75cf03a4c1cca0c55171583c284aca7b2c6562fc
Brandon Wallace <brandonwallace403@gmail.com>
2023-09-29 21:13:46 -0400

    running migrations

--- server/models.py
+++ server/models.py
@@ -4,3 +4,11 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class Movie(db.Model, SerializerMixin):
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Movie {self.title}>'
