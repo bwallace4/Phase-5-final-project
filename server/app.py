690af02aadd6a020da98cab1eb9c90b95dfd88e5
Brandon Wallace <brandonwallace403@gmail.com>
2023-10-02 02:27:20 -0400

    created backend with frontend

--- server/app.py
+++ server/app.py
@@ -5,69 +5,27 @@
 # Remote library imports
 from flask import request,jsonify,make_response
 from flask_restful import Resource
-from flask_marshmallow import Marshmallow
 # Local imports
 from config import app, db, api
 # Add your model imports
-from models import Users
-ma=Marshmallow(app)
+from models import User
+
 # Views go here!
-class UserSchema(ma.Schema):
-    class Meta:
-        fields = ('id','name','email','password')
-  
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
-  
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
- 
 @app.route('/users', methods=['GET']) 
-def listusers():
-    all_users = Users.query.all()
-    results = users_schema.dump(all_users)
-    return jsonify(results)
-  
-@app.route('/userdetails/<id>',methods =['GET'])
-def userdetails(id):
-    user = Users.query.get(id)
-    return user_schema.jsonify(user)
-  
-@app.route('/userupdate/<id>',methods = ['PUT'])
-def userupdate(id):
-    user = Users.query.get(id)
-  
-    name = request.json['name']
-    email = request.json['email']
-  
-    user.name = name
-    user.email = email
-  
+def get_users():
+    users = User.query.all()
+    return jsonify([user.to_dict() for user in users])
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    new_user = User(
+        username=data['username'],
+        email=data['email']
+    )
+    db.session.add(new_user)
     db.session.commit()
-    return user_schema.jsonify(user)
- 
-@app.route('/userdelete/<id>',methods=['DELETE'])
-def userdelete(id):
-    user = Users.query.get(id)
-    db.session.delete(user)
-    db.session.commit()
-    return user_schema.jsonify(user)
-  
-@app.route('/newuser',methods=['POST'])
-def newuser():
-    name = request.json['name']
-    email = request.json['email']
-    password = request.json['password']
-  
-    print(name)
-    print(email)
-    print(password)
- 
-    users = Users(name=name, email=email, password=password)
- 
-    db.session.add(users)
-    db.session.commit()
-    return user_schema.jsonify(users)
+    return jsonify(new_user.to_dict())
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -9,9 +9,10 @@ from config import db
 # Define the association table
 
          
-class Users(db.Model):
-    __tablename__ = "tblusers"
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(150), index=True, unique=True)
-    email = db.Column(db.String(150), index=True, unique=True)
-    password = db.Column(db.String(255), index=True, unique=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
