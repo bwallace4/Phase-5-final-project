5a683ec7a5db8e4568f83ee2f09728e2f6b9f665
Brandon Wallace <brandonwallace403@gmail.com>
2023-10-02 03:25:33 -0400

    added fetch routes to app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,28 +5,55 @@ import React, { useEffect, useState } from "react";
 function ItemList() {
   const [items, setItems] = useState([]);
   const [name, setName] = useState('');
+  const [editingItemId, setEditingItemId] = useState(null); // To track the item being edited
+  const [editedName, setEditedName] = useState(''); // To store the edited item name
 
   useEffect(() => {
-    fetch('/items')
+    fetch('http://127.0.0.1:5555/items')
       .then((response) => response.json())
       .then((data) => setItems(data));
   }, []);
 
   const handleCreateItem = () => {
-    fetch('/items', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name }),
-    })
-      .then((response) => response.json())
-      .then(() => {
-        setName('');
-        fetch('/items')
-          .then((response) => response.json())
-          .then((data) => setItems(data));
-      });
+    if (!editingItemId) {
+      // If not editing, create a new item
+      fetch('http://127.0.0.1:5555/items', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name }),
+      })
+        .then((response) => response.json())
+        .then(() => {
+          setName('');
+          fetch('/items')
+            .then((response) => response.json())
+            .then((data) => setItems(data));
+        });
+    } else {
+      // If editing, update the existing item
+      fetch(` http://127.0.0.1:5555/items`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: editedName }),
+      })
+        .then(() => {
+          setEditingItemId(null);
+          setEditedName('');
+          fetch('/items')
+            .then((response) => response.json())
+            .then((data) => setItems(data));
+        });
+    }
+  };
+
+  const handleEditItem = (id, itemName) => {
+    // Set the editing state when the "Edit" button is clicked
+    setEditingItemId(id);
+    setEditedName(itemName);
   };
 
   const handleDeleteItem = (id) => {
@@ -46,19 +73,41 @@ function ItemList() {
       <ul>
         {items.map((item) => (
           <li key={item.id}>
-            {item.name}
-            <button onClick={() => handleDeleteItem(item.id)}>Delete</button>
+            {item.id === editingItemId ? (
+              // Render the edit form when editing
+              <div>
+                <input
+                  type="text"
+                  placeholder="Edit item name"
+                  value={editedName}
+                  onChange={(e) => setEditedName(e.target.value)}
+                />
+                <button onClick={handleCreateItem}>Save</button>
+              </div>
+            ) : (
+              // Render the item name and buttons when not editing
+              <div>
+                {item.name}
+                <button onClick={() => handleEditItem(item.id, item.name)}>Edit</button>
+                <button onClick={() => handleDeleteItem(item.id)}>Delete</button>
+              </div>
+            )}
           </li>
         ))}
       </ul>
       <div>
-        <input
-          type="text"
-          placeholder="Item name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <button onClick={handleCreateItem}>Create</button>
+        {editingItemId === null && (
+          // Render the item creation form when not editing
+          <div>
+            <input
+              type="text"
+              placeholder="Item name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <button onClick={handleCreateItem}>Create</button>
+          </div>
+        )}
       </div>
     </div>
   );
