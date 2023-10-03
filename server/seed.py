4e6d4ff108481bc4e3f8f9d788762c7b265ab5de
Brandon Wallace <brandonwallace403@gmail.com>
2023-10-03 09:37:30 -0400

    successfully seeded my database

--- server/seed.py
+++ server/seed.py
@@ -5,13 +5,45 @@ from random import randint, choice as rc
 
 # Remote library imports
 from faker import Faker
+import bcrypt
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import bcrypt
 
 # Local imports
 from app import app
-from models import db
-
+from models import db,User
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =  'sqlite:///app.db'  # Replace with your database URI
+db = SQLAlchemy(app)
+
+# Import your User model here
+
+# Your seeding code
+with app.app_context():
+    # Seed the database with fake users
+    for _ in range(10):  # Change the number as needed
+        username = fake.user_name()
+        email = fake.email()
+        password = 'your_password_here'  # Replace with a password or generate one randomly
+
+        # Hash the password using bcrypt
+        salt = bcrypt.gensalt()
+        password_hash = bcrypt.hashpw(password.encode('utf-8'), salt).decode('utf-8')
+
+        # Create a new user instance and add it to the database
+        new_user = User(username=username, email=email, password_hash=password_hash)
+        db.session.add(new_user)
+
+    # Commit the changes to the database
+    db.session.commit()
+
+
+
