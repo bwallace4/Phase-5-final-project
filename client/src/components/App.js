c097e44de258ee9521fdeec276706578e8400f8f
Brandon Wallace <brandonwallace403@gmail.com>
2023-10-04 10:31:11 -0400

    created a navbar and styled it

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import RegisterForm from './RegisterForm';
 import SuccessPage from './SuccessPage';
-import UserList from './UserList'; // Import the UserList component
+import UserList from './UserList';
+import './Navbar.css'; 
 
 function App() {
   return (
     <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/user-list">User List</Link>
+          </li>
+          <li>
+            <Link to="/success-page">Success Page</Link>
+          </li>
+        </ul>
+      </nav>
+
       <Switch>
         <Route path="/success-page" component={SuccessPage} />
-        <Route path="/user-list" component={UserList} /> {/* Add this route */}
+        <Route path="/user-list" component={UserList} />
         <Route path="/" component={RegisterForm} />
       </Switch>
     </Router>
--- server/app.py
+++ server/app.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 from flask import request, jsonify, make_response
 from flask_restful import Resource
+
 # Local imports
 from config import app, db, api
+
 # Add your model imports
 from models import User, Tag, Post
 import json  # Import the json module
-
-
 
 
 class RegisterUserResource(Resource):
@@ -17,16 +17,25 @@ class RegisterUserResource(Resource):
         # Check if the required fields are present in the request data
         required_fields = ["username", "email", "password"]
         if not all(field in data for field in required_fields):
-            return json.dumps({"error": "Missing required fields"}), 400  # Use json.dumps here
+            return (
+                json.dumps({"error": "Missing required fields"}),
+                400,
+            )  # Use json.dumps here
 
         # Check if a user with the same username or email already exists
         existing_user = User.query.filter_by(username=data["username"]).first()
         if existing_user:
-            return json.dumps({"error": "Username is already taken"}), 400  # Use json.dumps here
+            return (
+                json.dumps({"error": "Username is already taken"}),
+                400,
+            )  # Use json.dumps here
 
         existing_email = User.query.filter_by(email=data["email"]).first()
         if existing_email:
-            return json.dumps({"error": "Email is already registered"}), 400  # Use json.dumps here
+            return (
+                json.dumps({"error": "Email is already registered"}),
+                400,
+            )  # Use json.dumps here
 
         try:
             # Create a new User instance and set the password directly
@@ -41,63 +50,29 @@ class RegisterUserResource(Resource):
             return json.dumps(response_data), 201  # Use json.dumps here
         except Exception as e:
             error_data = {"error": str(e)}
-            return json.dumps(error_data), 500  # Handle other exceptions gracefully and use json.dumps here
+            return (
+                json.dumps(error_data),
+                500,
+            )  # Handle other exceptions gracefully and use json.dumps here
+
 
 # Add the RegisterUserResource to your API
-api.add_resource(RegisterUserResource, '/register')
+api.add_resource(RegisterUserResource, "/register")
 
 
-
-     
 # Get all users
 @app.route("/users", methods=["GET"])
 def get_users():
     users = User.query.all()
-    
+
     if not users:
         return jsonify({"message": "No users found"}), 404
-    
+
     return jsonify([user.to_dict() for user in users]), 200
-
-# Update a specific user by ID
-@app.route("/update/<int:user_id>", methods=["PUT"])
-def update_user(user_id):
-    try:
-        data = request.json
-        username = data.get("username")
-        
-        if not username:
-            return jsonify({"error": "Username is required"}), 400
-
-        user_to_update = User.query.get(user_id)
-
-        if not user_to_update:
-            return jsonify({"error": "User not found"}), 404
-
-        # Check if the provided username matches the user's current username
-        if user_to_update.username != username:
-            return jsonify({"error": "You can only update your own account"}), 403
-
-        # You may want to add additional validation here for other fields if needed
-
-        # Update the user's information (e.g., username or password)
-        if "password" in data:
-            password = data["password"]
-            hashed_password = generate_password_hash(password, method="sha256")
-            user_to_update.password_hash = hashed_password
-
-        # Commit the changes to the database
-        db.session.commit()
-
-        return jsonify({"message": "User updated successfully"}), 200
-    except Exception as e:
-        # Log the error for debugging purposes
-        print(str(e))
-        return jsonify({"error": "An error occurred while updating the user"}), 500
 
 
 # Delete a user by username
-@app.route('/users/<int:user_id>', methods=['DELETE'])
+@app.route("/users/<int:user_id>", methods=["DELETE"])
 def delete_user(user_id):
     user = User.query.get(user_id)
 
@@ -108,7 +83,6 @@ def delete_user(user_id):
     db.session.commit()
 
     return jsonify({"message": "User deleted successfully"}), 200
-
 
 
 if __name__ == "__main__":
