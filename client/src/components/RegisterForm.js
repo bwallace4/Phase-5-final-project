bfdbb07bf2bdc1d67de4eb1a0328f0018e03a4f3
Brandon Wallace <brandonwallace403@gmail.com>
2023-10-05 18:58:40 -0400

    printing error messages to frontend

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -11,6 +11,8 @@ function RegisterForm() {
   });
 
   const [errors, setErrors] = useState({}); // Store validation errors
+  const [registrationStatus, setRegistrationStatus] = useState(''); // Store registration status message
+  const [serverResponseData, setServerResponseData] = useState(null); // Store server response data
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -19,6 +21,7 @@ function RegisterForm() {
       [name]: value,
     });
   };
+
   const handleRegistration = () => {
     // Send a POST request to /register with formData
     fetch('/register', {
@@ -30,18 +33,25 @@ function RegisterForm() {
     })
       .then((response) => response.json())
       .then((data) => {
+        console.log('Data received from server:', data);
+        // Store the server response data
+        setServerResponseData(data);
+
         if (data.error) {
           // Handle validation errors received from the server
-          setErrors(data); // Set errors in the state
+          setErrors(data);
+          setRegistrationStatus('Registration failed. Please fix the errors.');
         } else {
           // Clear any previous errors
           setErrors({});
           // Check if registration was successful
-          if (data.message === "User registered successfully") {
+          if (data.message === 'User registered successfully') {
+            setRegistrationStatus('Registration successful. Redirecting...');
             // Redirect the user to another page upon successful registration
-            history.push('/success-page');
+            setTimeout(() => {
+              history.push('/success-page');
+            }, 2000); // Redirect after 2 seconds
           }
-          // Optionally, you can show a success message or take other actions here
         }
       })
       .catch((error) => {
@@ -50,10 +60,11 @@ function RegisterForm() {
         // Optionally, you can set an error state here to inform the user
       });
   };
-  
+
   return (
     <div className="register-form-container">
       <h2>Register</h2>
+      {registrationStatus && <p className="registration-status">{registrationStatus}</p>}
       <div>
         <input
           type="text"
@@ -87,10 +98,13 @@ function RegisterForm() {
       <div>
         <button onClick={handleRegistration}>Register</button>
       </div>
+      {serverResponseData && (
+        <div className="server-response">
+          <p>Data received from server: {JSON.stringify(serverResponseData)}</p>
+        </div>
+      )}
     </div>
   );
 }
 
 export default RegisterForm;
-
-
