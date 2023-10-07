727180d2f18265e086ee2d0696623dbed9db1b79
Brandon Wallace <brandonwallace403@gmail.com>
2023-10-06 21:45:52 -0400

    updated the userlist css/js

--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -1,33 +1,33 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './UserList.css';
 
 function UserList() {
   const [users, setUsers] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const usersPerPage = 5; // Number of users to display per page
-  const indexOfLastUser = currentPage * usersPerPage;
-  const indexOfFirstUser = indexOfLastUser - usersPerPage;
-  const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser);
-
   const [isLoading, setIsLoading] = useState(false);
-  const [showUsers, setShowUsers] = useState(false);
 
-  const getUsers = async () => {
-    setIsLoading(true);
-    try {
-      const response = await fetch('/users'); // Replace with your API endpoint
-      if (!response.ok) {
-        throw new Error('Failed to fetch users');
+  useEffect(() => {
+    const getUsers = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch('/users'); // Replace with your API endpoint
+        if (!response.ok) {
+          throw new Error('Failed to fetch users');
+        }
+        const data = await response.json();
+        // Sort users by ID in ascending order
+        data.sort((a, b) => a.id - b.id);
+        setUsers(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
       }
-      const data = await response.json();
-      setUsers(data);
-      setShowUsers(true);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+    };
+
+    getUsers(); // Load users when the component mounts
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // Empty dependency array to run the effect only once
 
   const handleDeleteUser = async (id) => {
     try {
@@ -45,38 +45,35 @@ function UserList() {
     }
   };
 
-  const paginate = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
-
   return (
     <div className="user-list-container">
       <h2>User List</h2>
-      <button onClick={getUsers} disabled={isLoading}>
-        {isLoading ? 'Loading...' : 'Get Users'}
-      </button>
-      {showUsers && (
-        <>
-          <ul className="user-list">
-            {currentUsers.map((user) => (
-              <li key={user.id}>
-                <span className="user-username">{user.username}</span>
-                <button onClick={() => handleDeleteUser(user.id)}>Delete</button>
-              </li>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <table className="user-table">
+          <thead>
+            <tr>
+              <th>Number</th>
+              <th>Username</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user.id}>
+                <td>{index + 1}</td>
+                <td>{user.username}</td>
+                <td>
+                  <button onClick={() => handleDeleteUser(user.id)}>Delete</button>
+                </td>
+              </tr>
             ))}
-          </ul>
-          <div className="pagination">
-            {Array.from({ length: Math.ceil(users.length / usersPerPage) }).map((_, index) => (
-              <button key={index} onClick={() => paginate(index + 1)}>
-                {index + 1}
-              </button>
-            ))}
-          </div>
-        </>
+          </tbody>
+        </table>
       )}
     </div>
   );
 }
 
 export default UserList;
-
